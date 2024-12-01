851357514ed832bc5f43da6987c242e6c5abfa35
Kasper <805006+kasper573@users.noreply.github.com>
2024-12-01 20:18:39 +0100

    group into parts

--- day1.test.ts
+++ day1.test.ts
@@ -1,18 +1,21 @@
+import { describe, test } from "jsr:@std/testing/bdd";
 import { assertEquals } from "jsr:@std/assert";
 import { day1 } from "./day1.ts";
 
 const textDecoder = new TextDecoder("utf-8");
 
-Deno.test("example", async () => {
-  const file = await Deno.readFile("./day1.example.txt");
-  const contents = textDecoder.decode(file);
-  const result = day1(contents);
-  assertEquals(result, 11);
-});
+describe("part 1", () => {
+  test("example", async () => {
+    const file = await Deno.readFile("./day1.example.txt");
+    const contents = textDecoder.decode(file);
+    const result = day1(contents);
+    assertEquals(result, 11);
+  });
 
-Deno.test("input", async () => {
-  const file = await Deno.readFile("./day1.input.txt");
-  const contents = textDecoder.decode(file);
-  const result = day1(contents);
-  assertEquals(result, 2000468);
+  test("input", async () => {
+    const file = await Deno.readFile("./day1.input.txt");
+    const contents = textDecoder.decode(file);
+    const result = day1(contents);
+    assertEquals(result, 2000468);
+  });
 });
