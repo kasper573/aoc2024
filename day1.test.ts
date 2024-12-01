b455e3f70c2ae0a2cb0009278f69185f87b9ba14
Kasper <805006+kasper573@users.noreply.github.com>
2024-12-01 20:26:14 +0100

    preload input

--- day1.test.ts
+++ day1.test.ts
@@ -3,16 +3,18 @@ import { assertEquals } from "jsr:@std/assert";
 import { day1_lists, day1_sum } from "./day1.ts";
 
 const textDecoder = new TextDecoder("utf-8");
+const example = await readUTF8("./day1.example.txt");
+const input = await readUTF8("./day1.input.txt");
 
 describe("part 1: sum", () => {
-  test("example", async () => {
-    const lists = await readUTF8("./day1.example.txt").then(day1_lists);
+  test("example", () => {
+    const lists = day1_lists(example);
     const sum = day1_sum(lists);
     assertEquals(sum, 11);
   });
 
-  test("input", async () => {
-    const lists = await readUTF8("./day1.input.txt").then(day1_lists);
+  test("input", () => {
+    const lists = day1_lists(input);
     const result = day1_sum(lists);
     assertEquals(result, 2000468);
   });
